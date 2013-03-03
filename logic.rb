b4a7ebcc778819f268611ece497948fb1078ce2f
Chris Metcalfe <chrsmtclf@gmail.com>
2013-03-03 18:23:18 +0000

    Reads temperature sensor via lm-sensors
    Previously read temperature from stdin for manual testing purposes. Now
    uses linux lm-sensors package command `sensors -f` and awk to get
    temperature.

--- logic.rb
+++ logic.rb
@@ -9,7 +9,11 @@ RELAYS = Relays.new
 THERMOSTAT = Thermostat.new
 
 while true do
-	reading = gets.chomp.to_f
+	#reading = gets.chomp.to_f
+	reading = `sensors -f | grep temp1 | awk '{print $2}'`[/[0-9]{1,3}\.[0-9]+/].to_f
 	THERMOSTAT.check_temperature reading
+	puts "Current temperature: #{reading}"
 	puts RELAYS.inspect
+	puts ""
+	sleep 10
 end
